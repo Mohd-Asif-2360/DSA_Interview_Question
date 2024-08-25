3bdc6bbcd69ae092ded53d246a7730390f2e5e33
Mohammad Asif <mohdasifreal@gmail.com>
2024-08-25 12:23:20 +0530

    ne changes

--- DSA Prep/ArrayProvider.cs
+++ DSA Prep/ArrayProvider.cs
@@ -16,11 +16,13 @@ namespace DSA_Prep
             //Facotorial_Of_Big_Number.Factorial(5);
 
             //Get Max Product subaaray
-            Maximum_Product_SubArray.Max_Product_SubArray(new int[] { 2, 3, -2, 4});
+            Maximum_Product_SubArray.Max_Product_SubArray([2, 3, -2, 4]);
 
 
             //get longest consecutive subsequence
-            Longest_Consecutive_SubSequence.Longest_Sonsecutive(new int[] {1, 1,  1, 8, 7, 9, 3, 10, 4, 20, 2, 6, 5});
+            Longest_Consecutive_SubSequence.Longest_Sonsecutive([1, 1,  1, 8, 7, 9, 3, 10, 4, 20, 2, 6, 5]);
+
+            Elements_nby3_times.FindElements_NBy3_Times([1, 2, 1, 2, 1, 1]);
         }
     }
 }
